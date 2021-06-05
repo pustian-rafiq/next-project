f0431abb7af18fad9e6f6ee6eb5d85c093860255
Md.Rafiqul <rafiqul.pust.cse@gmail.com>
2021-06-05 10:41:55 +0600

    add progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,17 @@ import '../styles/fontawesome.css'
 import NextNprogress from 'nextjs-progressbar';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextNprogress
+          color="#CB4D85"
+          startPosition={0.3}
+          stopDelayMs={200}
+          height="3"
+      />
+      <Component {...pageProps} />
+    </>
+)
 }
 
 export default MyApp
--- pages/category.js
+++ pages/category.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-
+import NavMenuDesktop from '../components/NavMenuDesktop';
 class category extends Component {
     render() {
         return (
             <div>
-                
+                <NavMenuDesktop/>
             </div>
         );
     }
