65728d38b90d6a4cf2b920591612c962ef862539
Md.Rafiqul <rafiqul.pust.cse@gmail.com>
2021-06-06 11:15:31 +0600

    category list

--- pages/category.js
+++ pages/category.js
@@ -1,10 +1,54 @@
+
+export async function getStaticProps() {
+    const data = [
+        {
+            id: 1,
+            cat_name: 'Laptop',
+            cat_code: 'Laptop110',
+            cat_icon: '/laptop.svg',
+
+        },
+        {
+            id: 3,
+            cat_name: 'Laptop',
+            cat_code: 'Laptop110',
+            cat_icon: '/laptop.svg',
+        },
+        {
+            id: 4,
+            cat_name: 'Laptop',
+            cat_code: 'Laptop110',
+            cat_icon: '/laptop.svg',
+        },
+        {
+            id: 6,
+            cat_name: 'Laptop',
+            cat_code: 'Laptop110',
+            cat_icon: '/laptop.svg',
+        },
+
+    ]
+
+    return {
+        props:{
+            dataTable:data,
+        }
+    }
+
+
+}
+
+
+
 import React, { Component } from 'react';
 import NavMenuDesktop from '../components/NavMenuDesktop';
+import CategoryList from '../components/CategoryList';
 class category extends Component {
     render() {
         return (
             <div>
                 <NavMenuDesktop/>
+                <CategoryList dataTable={this.props.dataTable}/>
             </div>
         );
     }
