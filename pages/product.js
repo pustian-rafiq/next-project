6cc1fca2da5026dc8151ee2c55af792c706ce926
Md.Rafiqul <rafiqul.pust.cse@gmail.com>
2021-06-06 16:32:19 +0600

    report and product

--- pages/product.js
+++ pages/product.js
@@ -1,11 +1,66 @@
+
+export async function getStaticProps() {
+    const data = [
+        {
+            id: 1,
+            product_name: 'Laptop',
+            product_code: 'Laptop110',
+            product_icon: '/laptop.svg',
+            product_price: '10,000',
+            product_category: 'Laptop',
+            product_remarks: 'Asus',
+
+        },
+        {
+            id: 4,
+            product_name: 'Laptop',
+            product_code: 'Laptop110',
+            product_icon: '/laptop.svg',
+            product_price: '10,000',
+            product_category: 'Laptop',
+            product_remarks: 'Asus',
+        },
+        {
+            id: 6,
+            product_name: 'Laptop',
+            product_code: 'Laptop110',
+            product_icon: '/laptop.svg',
+            product_price: '10,000',
+            product_category: 'Laptop',
+            product_remarks: 'Asus',
+        },
+        {
+            id: 10,
+            product_name: 'Laptop',
+            product_code: 'Laptop110',
+            product_icon: '/laptop.svg',
+            product_price: '10,000',
+            product_category: 'Laptop',
+            product_remarks: 'Asus',
+        },
+
+    ]
+
+    return {
+        props:{
+            dataTable:data,
+        }
+    }
+
+
+}
+
+
 import React, { Component } from 'react';
 import NavMenuDesktop from '../components/NavMenuDesktop';
+import ProductList from '../components/ProductList';
 
 class product extends Component {
     render() {
         return (
             <div>
                 <NavMenuDesktop/>
+                <ProductList dataTable={this.props.dataTable}/>
             </div>
         );
     }
--- pages/report.js
+++ pages/report.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import NavMenuDesktop from '../components/NavMenuDesktop'
+import ReportList from '../components/ReportList'
 
 export default class report extends Component {
     render() {
         return (
             <div>
                 <NavMenuDesktop/>
+                <ReportList/>
             </div>
         )
     }
